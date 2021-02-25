f7b5d364f39de8432eb97ca1b366b40bdd75cf47
bb2567 <marksong0976@gmail.com>
2021-02-25 21:15:53 +0800

    material test

---
+++ src/components/LoginBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Button} from "@material-ui/core"
+
+export default class LoginBox extends Component {
+    render() {
+        return (
+            <>
+                <Button variant="contained">Hello</Button>
+            </>
+        )
+    }
+}
